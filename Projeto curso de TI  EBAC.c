34965b6de3ead336f3ecfc7d92bc1e39c582cc01
Facundo Atahides <facundoatahides01@gmail.com>
2024-09-16 16:48:01 -0300

    Update Projeto curso de TI  EBAC.c

--- Projeto curso de TI  EBAC.c
+++ Projeto curso de TI  EBAC.c
@@ -1,18 +1,18 @@
-#include <stdio.h> //Bilbioteca de comunicaÁ„o com o usu·rio
-#include <stdlib.h> //Biblioteca de alocaÁ„o de espaÁo em memÛria
-#include <locale.h> //Biblioteca de alocaÁ„o de texto por regi„o
+#include <stdio.h> //Bilbioteca de comunica√ß√£o com o usu√°rio
+#include <stdlib.h> //Biblioteca de aloca√ß√£o de espa√ßo em mem√≥ria
+#include <locale.h> //Biblioteca de aloca√ß√£o de texto por regi√£o
 #include <string.h> //biblioteca responsavel por cuidar das strings
 
-int registro() //FUN«√O responsavel por cadastrar os usu·rios no sistema
+int registro() //FUN√á√ÉO responsavel por cadastrar os usu√°rios no sistema
 {
-     //Inicio criaÁ„o de vari·veis/string
+     //Inicio cria√ß√£o de vari√°veis/string
      char arquivo[40];
 	 char cpf[40];
      char nome[40];
      char sobrenome[40];
      char cargo[40];
 
-     //Final criaÁ„o de vari·veis/string
+     //Final cria√ß√£o de vari√°veis/string
 
      printf("Digite seu cpf: ");
      scanf("%s", cpf); //%s refere-se a string
@@ -86,12 +86,12 @@ int consulta()
 
 	if(file == NULL)
     {
-        printf("CPF n„o localizado\n");
+        printf("CPF n√£o localizado\n");
     }
 
      while(fgets(conteudo, 200, file)!= NULL)
      {
-         printf("\tEssas s„o as informaÁıes do usuario:\n");
+         printf("\tEssas s√£o as informa√ß√µes do usuario:\n");
          printf("\t%s", conteudo);
          printf("\n\n");
      }
@@ -109,7 +109,7 @@ int deletar()
    file = fopen(cpf, "r");
 
    if(file == NULL) {
-    printf("CPF n„o encontrado\n");
+    printf("CPF n√£o encontrado\n");
     system("pause");
     return 0;
    }
@@ -148,24 +148,25 @@ int main()
 	    setlocale(LC_ALL, "portuguese"); //Definindo a Liguagem
 
 
-		printf("## CartÛrio da EBAC ###\n\n"); //Inicio do menu
-	    printf("Escolha a opÁ„o desejada do menu: \n\n");
+		printf("## Cart√≥rio da EBAC ###\n\n"); //Inicio do menu
+	    printf("Escolha a op√ß√£o desejada do menu: \n\n");
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - Consultar nomes\n");
 	    printf("\t3 - Deletar nomes\n\n");
-	    printf("OpÁ„o: "); //fim do menu
+	    printf("\t4 - Sair do sistema\n");
+	    printf("Op√ß√£o: "); //fim do menu
 
-        scanf("%d" , &opcao); //arÈa,armazenando a escolha do usu·rio
+        scanf("%d" , &opcao); //ar√©a,armazenando a escolha do usu√°rio
 
         system("cls"); //responsavel por limpar a tela
 
 
 
 
-        switch(opcao) //inicio da seleÁ„o do menu
+        switch(opcao) //inicio da sele√ß√£o do menu
         {
          	 case 1:
-  	        registro(); //chamada de funÁıes
+  	        registro(); //chamada de fun√ß√µes
              break;
 
              case 2:
@@ -176,8 +177,13 @@ int main()
 	        deletar();
    	        break;
 
+		case 4:
+		printf("Obrigado por utilizar o nosso sistema");
+		return 0;
+		break;
+
    	         default:
-   	         printf("Essa opÁ„o n„o est· disponivel ");
+   	         printf("Essa op√ß√£o n√£o est√° disponivel ");
    	         system ("pause");
 			 break;
 
